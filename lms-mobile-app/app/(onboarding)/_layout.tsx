814f3f6f94060c07ca773fd05bed2b62db470afa
ThanhTung1909 <phamthanhtung200698@gmail.com>
2025-10-25 09:22:43 +0700

    feat: create splash screen

--- lms-mobile-app/app/(onboarding)/_layout.tsx
+++ lms-mobile-app/app/(onboarding)/_layout.tsx
@@ -3,9 +3,5 @@ import { useEffect } from "react";
 
 export default function OnboardingLayout() {
   const router = useRouter();
-
-  // Có thể điều hướng từng màn theo button "Tiếp tục" hoặc auto sau timeout ở mỗi screen.
-  // Hoặc bạn chỉ cần <Slot/> thôi nếu điều hướng nằm trong từng màn hình.
-
   return <Slot />;
 }
--- lms-mobile-app/app/(onboarding)/splash.tsx
+++ lms-mobile-app/app/(onboarding)/splash.tsx
@@ -1,20 +1,64 @@
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 export default function SplashScreen() {
   const router = useRouter();
+  const opacity = useSharedValue(0);
+  const scale = useSharedValue(0.8);
+  const translateY = useSharedValue(20);
 
   useEffect(() => {
+    opacity.value = withTiming(1, {
+      duration: 1000,
+      easing: Easing.out(Easing.exp),
+    });
+    scale.value = withTiming(1, {
+      duration: 1000,
+      easing: Easing.out(Easing.exp),
+    });
+    translateY.value = withTiming(0, {
+      duration: 1000,
+      easing: Easing.out(Easing.exp),
+    });
+
     const timer = setTimeout(() => {
       router.replace("/(onboarding)/onboarding1");
     }, 1500);
     return () => clearTimeout(timer);
   }, []);
 
+  const animatedLogo = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    transform: [{ scale: scale.value }, { translateY: translateY.value }],
+  }));
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Splash Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.Image
+        source={require("../../src/assets/images/Cybex_logo_1.png")}
+        style={[styles.logo, animatedLogo]}
+        resizeMode="contain"
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  logo: {
+    width: 160,
+    height: 160,
+  },
+});
